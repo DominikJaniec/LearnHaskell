c261e5ead0820df8c8dc526098c26b4e1910bc0c
Dominik Janiec <dominikjaniec@gmail.com>
2017-01-28 23:03:34 +0100

    Chapter: 2. Starting Out
    http://learnyouahaskell.com/starting-out

---
+++ repl/baby.hs
@@ -0,0 +1,16 @@
+doubleMe x = x + x
+
+-- doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else doubleMe x
+
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUpperCase str = [c | c <- str, c `elem` ['A'..'Z']]
+
+rightTriangls maxSide permiter = [(a,b,c) | c <- [1..maxSide], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == permiter]
